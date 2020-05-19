12bd7318b5c97ce1d404e230b080cf188f581b7c
Lucky Manaya <16189@bayfield-high.school.nz>
2020-05-20 11:39:44 +1200

    Add project files.

---
+++ Files/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.txtAddInfo = new System.Windows.Forms.TextBox();
+            this.lblData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(35, 85);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(328, 38);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // txtAddInfo
+            // 
+            this.txtAddInfo.Location = new System.Drawing.Point(23, 38);
+            this.txtAddInfo.Name = "txtAddInfo";
+            this.txtAddInfo.Size = new System.Drawing.Size(100, 20);
+            this.txtAddInfo.TabIndex = 2;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(348, 85);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(35, 13);
+            this.lblData.TabIndex = 3;
+            this.lblData.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 300);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.txtAddInfo);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox txtAddInfo;
+        private System.Windows.Forms.Label lblData;
+    }
+}
+
---
+++ Files/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            string info = txtAddInfo.Text;
+            System.IO.File.WriteAllLines(@"C:", info);
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:", true))
+            {
+                file.WriteLine(info);
+            }
+
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string text = System.IO.File.ReadAllText(@"C:");
+            lblData.Text = text;
+            
+            string[] lines = System.IO.File.ReadAllLines(@"C:");
+        for(int i =- 0; i < lines.Length; i++)
+            {
+                lblData.Text += "\n" + lines[i];
+            }
+
+        }
+    }
+}
